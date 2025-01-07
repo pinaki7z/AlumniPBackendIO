21b8ac4d2dd63a841a7111f5b35c85e12f16df48
Pinaki <pinakiprasadrajkumar@gmail.com>
2025-01-07 17:58:10 +0530

    db string change

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose')
 // 54.242.180.165
-mongoose.connect('mongodb://testUser:222222111@54.242.12.119:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://testUser:>)d1oOWD,0N0@54.242.12.119:27017/admin', { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
