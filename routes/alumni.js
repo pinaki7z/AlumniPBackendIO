11b65fa87fc1a9d51571f37a1c18a4c4cd0d1940
Pinaki <pinakiprasadrajkumar@gmail.com>
2025-02-07 16:35:52 +0530

    coverpicture

--- routes/alumni.js
+++ routes/alumni.js
@@ -509,7 +509,7 @@ alumniRoutes.get("/all", async (req, res) => {
   try {
     const alumni = await Alumni.find()
       .select(
-        "firstName lastName profilePicture profileLevel _id email workExperience accountDeleted graduatingYear class groupNames linkedIn department batch aboutMe followers following country city"
+        "firstName lastName profilePicture profileLevel _id email workExperience accountDeleted graduatingYear class groupNames linkedIn department batch aboutMe followers following country city coverPicture"
       )
       .lean();
     if (!alumni.length) {
